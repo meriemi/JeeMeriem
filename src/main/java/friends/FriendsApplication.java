3459dfc55ac024cf726c701d5e6904a87a60ac4a
meriemi <meriem.farouj@gmail.com>
2017-04-01 19:48:54 +0200

    #first push

---
+++ src/main/java/friends/FriendsApplication.java
@@ -0,0 +1,12 @@
+package friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsApplication.class, args);
+	}
+}
